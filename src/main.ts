87deeb1b86658ed3a4b6bb95346ab046ef0b4ee0
mzkmnk <sma0729mex@gmail.com>
2025-03-25 14:28:43 +0900

    feat: formControl demo

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { setThrowInvalidWriteToSignalError } from '@angular/core/primitives/signals';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
+  imports:[ FormsModule,ReactiveFormsModule ],
   template: `
     <h1>Hello from {{ name }}!</h1>
+    
     <a target="_blank" href="https://angular.dev/overview">
       Learn more about Angular
     </a>
+
+    <button (click)="addGuest()">add Guest</button>
+
+    <p>minlength:requiredLength: {{ formErrorMessages()?.['minlength'].requiredLength }}</p>
   `,
 })
 export class App {
   name = 'Angular';
+
+  form = new FormControl(new Set([1]),[
+    Validators.minLength(4),
+    Validators.maxLength(6)
+  ])
+
+  formErrorMessages = toSignal(
+    this.form.valueChanges.pipe(
+      tap(() => console.log(this.form.errors)),
+      map((val) => this.form.errors)
+    )
+  )
+
+  addGuest():void {
+    const value = new Set(this.form.value);
+    
+    value.add(value.size + 1)
+
+    this.form.setValue(value);
+  }
 }
 
 bootstrapApplication(App);
